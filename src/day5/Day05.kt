be8ea1d1968152ab616254bd45518e68777f7f45
Sebastic <webmaster@sebastic.com>
2024-12-05 19:41:32 +0100

    day 5 part 2

--- src/day5/Day05.kt
+++ src/day5/Day05.kt
@@ -7,15 +7,12 @@ fun main() {
 
     var splitOnEmptyLine = input.split("\n\n")
     var orderingRules = splitOnEmptyLine[0].split("\n")
-        .map{
+        .map {
             it.split('|')
         }
         .toList()
-        .groupBy({it[0].toInt()}, {it[1].toInt()})
+        .groupBy({ it[0].toInt() }, { it[1].toInt() })
 
-
-
-        //.associate { it.split('|').let { it[0].toInt() to it[1].toInt() } }
 
     var updates = splitOnEmptyLine[1]
         .split("\n")
@@ -29,41 +26,67 @@ fun main() {
     var resultPart1 = part1(orderingRules, updates)
     println("Part 1: $resultPart1")
 
-    var resultPart2 = part2(input)
+    var resultPart2 = part2(orderingRules, updates)
     println("Part 2: $resultPart2")
 
 }
 
 fun part1(orderingRules: Map<Int, List<Int>>, updates: List<List<Int>>): Int {
     var result = 0
-    updates.forEach {
-        update ->
-        if(isCorrectOrder(orderingRules, update)) {
+    updates.forEach { update ->
+        if (isCorrectOrder(orderingRules, update)) {
             val middleNumber = findMiddleNumber(update)
-            println("${update} has middle number: $middleNumber")
             result += middleNumber
         }
     }
     return result
 }
 
-fun findMiddleNumber(update: List<Int>) : Int{
-    val middle = update.size / 2 + 1
-    return update[middle-1]
+fun part2(orderingRules: Map<Int, List<Int>>, updates: List<List<Int>>): Int {
+    var result = 0
+    updates.forEach { update ->
+        if (!isCorrectOrder(orderingRules, update)) {
+            var correctOrder = orderList(orderingRules, update)
+            val middleNumber = findMiddleNumber(correctOrder)
+            result += middleNumber
+        }
+    }
+    return result
 }
 
-fun isCorrectOrder(orderingRules: Map<Int, List<Int>>, update: List<Int>
+fun orderList(orderingRules: Map<Int, List<Int>>, update: List<Int>): List<Int> {
+   return update.sortedWith(object: Comparator<Int> {
+        override fun compare(first: Int, second: Int): Int {
+            var rulesFirst = orderingRules[first]
+            if(rulesFirst != null){
+                if(rulesFirst.contains(second)) {
+                    return -1
+                }
+            }
+            var rulesSecond = orderingRules[second]
+            if(rulesSecond != null){
+                if(rulesSecond.contains(first)) {
+                    return 1
+                }
+            }
+            return 0
+        }}).toList()
+}
+
+
+fun findMiddleNumber(update: List<Int>): Int {
+    val middle = update.size / 2 + 1
+    return update[middle - 1]
+}
+
+fun isCorrectOrder(
+    orderingRules: Map<Int, List<Int>>, update: List<Int>
 ): Boolean {
-    println(" ")
-    println(" ")
-    println("checking update: $update")
     for (i in 0 until update.size) {
-      var rules = orderingRules[update[i]]
-        if(rules != null) {
-            println("Found rules for ${update[i]} : $rules")
-            for(j in rules.indices) {
-                if(update.contains(rules[j]) && update.indexOf(rules[j]) < i ) {
-                    println("update $update not valid because ${rules[j]} should be after ${update[i]} ")
+        var rules = orderingRules[update[i]]
+        if (rules != null) {
+            for (j in rules.indices) {
+                if (update.contains(rules[j]) && update.indexOf(rules[j]) < i) {
                     return false
                 }
             }
@@ -72,9 +95,6 @@ fun isCorrectOrder(orderingRules: Map<Int, List<Int>>, update: List<Int>
     return true
 }
 
-fun part2(matrix: String): Int {
-    return 1
-}
 
 
 
